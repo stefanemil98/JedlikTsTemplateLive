5716952a0c485627a430a60bb294e2308c95e262
stefanemil98 <36262683+stefanemil98@users.noreply.github.com>
2018-02-08 11:27:00 +0100

    Add files via upload

---
+++ ts/content.ts
@@ -0,0 +1,14 @@
+﻿import * as http from "http";
+import * as url from "url"; // űrlapokhoz, input kiolvasás
+import * as fs from "fs"; // file-kezelés
+
+export class Content {
+
+    Content(req: http.ServerRequest, res: http.ServerResponse): void {
+        res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+        res.write("<p>Hello TypeScript!</p>");
+
+        res.end();
+    }
+}
+
